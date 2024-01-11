312321914bceddecd78df161538d64336a30d269
dev-jhon-yo <dev.jhon.ss@gmail.com>
2024-01-11 11:14:05 -0300

    feat: using observers

--- src/client/observer.client.lua
+++ src/client/observer.client.lua
@@ -6,3 +6,16 @@ local Value = Fusion.Value
 local Observer = Fusion.Observer
 
 local health = Value(100)
+
+-- This observer will watch `health` for changes:
+local observer = Observer(health)
+
+local disconnect = observer:onChange(function()
+	print("The new value is: ", health:get())
+end)
+
+health:set(10)
+
+--* disconnect the above handler after 5 seconds
+task.wait(5)
+disconnect()
