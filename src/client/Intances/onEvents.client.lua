3e52ec2fc0c8d7c1db25c95fb87ad2f5207e7b0f
dev-jhon-yo <dev.jhon.ss@gmail.com>
2024-01-14 12:45:02 -0300

    ref: change file name and delete unused variable

--- src/client/Intances/onEvents.client.lua
+++ src/client/Intances/onEvents.client.lua
@@ -1,5 +1,4 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local StarterGui = game:GetService("StarterGui")
 local Fusion = require(ReplicatedStorage.Packages.Fusion)
 
 local New = Fusion.New
